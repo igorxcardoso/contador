38642ccd801e5a09d9a95b3f5b4a84cca1b908ce
Igor Cardoso <iygorsilva@gmail.com>
2022-04-27 08:54:45 -0300

    Row and TextButton widget; button creation

--- lib/main.dart
+++ lib/main.dart
@@ -18,14 +18,22 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  void decrement() {
+    print('decrement');
+  }
+
+  void increment() {
+    print('increment');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black38,
+      backgroundColor: Colors.red,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center, // end, start ou center
-        children: const [
-          Text(
+        children: [
+          const Text(
             'Pode entrar!',
             style: TextStyle(
               fontSize: 40,
@@ -33,12 +41,37 @@ class HomePage extends StatelessWidget {
               fontWeight: FontWeight.w700, // w800
             ),
           ),
-          Text(
+          const Text(
             '0',
             style: TextStyle(
               fontSize: 120,
               color: Colors.white,
             ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: decrement,
+                child: const Text(
+                  'Saiu',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: increment,
+                child: const Text(
+                  'Entrou',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
